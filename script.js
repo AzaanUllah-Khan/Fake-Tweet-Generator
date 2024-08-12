9104f4b7ace88432ba0d05f2161c678203c28041
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-08-12 22:47:31 +0500

    profile pic added though js feature added

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ Array.from(inputs).forEach(element => {
     element.addEventListener('input', start);
 });
 
-function start(){
-    document.getElementById('nameX').textContent = document.getElementById('name').value
-    document.getElementById('unameX').textContent = `@${document.getElementById('uname').value.toLowerCase()}`
-    document.getElementById('tweetText').textContent = document.getElementById('tt').value
+function start() {
+    const fileInput = document.getElementById('pp');
+    if (fileInput.files && fileInput.files[0]) {
+        const file = fileInput.files[0];
+        const imgUrl = URL.createObjectURL(file);
+        document.getElementById('profilePic').src = imgUrl;
+    }
+    
+    document.getElementById('nameX').textContent = document.getElementById('name').value;
+    document.getElementById('unameX').textContent = `@${document.getElementById('uname').value.toLowerCase()}`;
+    document.getElementById('tweetText').textContent = document.getElementById('tt').value;
 }
